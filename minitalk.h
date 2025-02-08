4852f1567a4f65dbbea4c2cd2ba11ff1f8682a74
Razan Srour <razanmsrour@gmail.com>
2025-02-08 16:50:35 +0300

    include libft headers

--- minitalk.h
+++ minitalk.h
@@ -6,10 +6,12 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 20:30:23 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/06 20:31:15 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/08 16:06:50 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include "libft/header/ft_printf.h"
+#include "libft/header/libft.h"
