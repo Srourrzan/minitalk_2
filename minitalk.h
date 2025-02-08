fc4aceb53469b6c89f37c79f23b580e9426e93fa
Razan Srour <razanmsrour@gmail.com>
2025-02-08 17:38:22 +0300

    define header guard

--- minitalk.h
+++ minitalk.h
@@ -6,9 +6,12 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 20:30:23 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/08 17:35:30 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/08 17:37:32 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
 
 #include <unistd.h>
 #include <signal.h>
@@ -21,3 +24,5 @@
 # endif
 
 void    ft_send_message(int pid, char *message);
+
+#endif
