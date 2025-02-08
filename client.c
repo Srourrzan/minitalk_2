1c3378fd6da4bf49b0ef85c26305d8cbba94083e
Razan Srour <razanmsrour@gmail.com>
2025-02-08 17:36:51 +0300

    create message sender function

--- client.c
+++ client.c
@@ -1,22 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   clinet.c                                           :+:      :+:    :+:   */
+/*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 20:30:05 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/08 15:59:28 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/08 17:35:15 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void    ft_send_message(int pid, char *message)
+{
+    int     iter;
+
+    iter = 0;
+    while (message[iter])
+    {
+        ft_send_char(pid, message[iter]);
+        iter++;
+    }
+    ft_send_char(pid, '\0');
+}
+
 int     main(int argc, char **argv)
 {
+    int     pid;
+    
+    pid = ft_atoi(argv[1]);
     if (argc == 3)
     {
-        ft_printf("pid = %d\n", argv[1]);
+        ft_printf("pid = %d\n", pid);
+        ft_printf("message = %s\n", argv[2]);
     }
+    ft_send_message(pid, argv[3]);
     return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: rsrour <rsrour@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/06 20:30:23 by rsrour            #+#    #+#             */
-/*   Updated: 2025/02/08 16:06:50 by rsrour           ###   ########.fr       */
+/*   Updated: 2025/02/08 17:35:30 by rsrour           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,3 +15,9 @@
 #include <stdlib.h>
 #include "libft/header/ft_printf.h"
 #include "libft/header/libft.h"
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000000
+# endif
+
+void    ft_send_message(int pid, char *message);
